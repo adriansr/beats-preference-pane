8c51aa9945999f8c4343c883a3dfeb74d05b2b86
Adrian Serrano <adrisr83@gmail.com>
2018-02-08 15:18:07 +0100

    Initial Commit

---
+++ beats-preference-pane/beats_preference_pane.h
@@ -0,0 +1,15 @@
+//
+//  beats_preference_pane.h
+//  beats-preference-pane
+//
+//  Created by Adrian Serrano on 08/02/2018.
+//  Copyright © 2018 Elastic. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface beats_preference_pane : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ beats-preference-pane/beats_preference_pane.m
@@ -0,0 +1,17 @@
+//
+//  beats_preference_pane.m
+//  beats-preference-pane
+//
+//  Created by Adrian Serrano on 08/02/2018.
+//  Copyright © 2018 Elastic. All rights reserved.
+//
+
+#import "beats_preference_pane.h"
+
+@implementation beats_preference_pane
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
